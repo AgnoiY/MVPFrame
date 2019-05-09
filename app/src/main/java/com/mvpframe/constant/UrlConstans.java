3efc1b1c472d15c12b696977a9f125649efae04a
changjunyong <changjunyong@worken.cn>
2019-05-09 13:04:18 +0800

    permissions

--- app/src/main/java/com/mvpframe/constant/UrlConstans.java
+++ app/src/main/java/com/mvpframe/constant/UrlConstans.java
@@ -14,14 +14,12 @@ public class UrlConstans {
      * 服务器地址
      */
     private static String DEF_TEST_SERVER = "http://183.129.178.44:8320/klApp/";//测试环境
-    private static String DEF_RELEASE_SERVER = "http://183.129.178.44:8320/klApp/";//测试环境
+    private static String DEF_RELEASE_SERVER = "http://183.129.178.44:8320/klApp/";//正式环境
 
     public static final String BASESERVER = BuildConfig.DEBUG ? DEF_TEST_SERVER : DEF_RELEASE_SERVER;
 
     /**
      * 用户登陆
      */
-//    public static final String LOGIN = "login.do";
     public static final String LOGIN = "user/loginByUserNamePassword";
-    public static final String selector = "selector/storeType";
 }
